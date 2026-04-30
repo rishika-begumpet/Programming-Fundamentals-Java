3eee595efa6e6f118974f12a0a37fde0a68c1854
rishika-begumpet <begumpetrishika@gmail.com>
2026-05-01 01:29:41 +0530

    ✅ Add solution for: Check if a year is a leap year or not.

---
+++ exercises/Check-if-a-year-is-a-leap-year-or-not..java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        if (year % 4 == 0 && year % 100 != 0) {
+            System.out.println("Leap year");
+        } else if(year % 400 == 0){
+        System.out.println("Leap year");
+       } else{
+            System.out.println("Not a leap year");
+        }
+           scanner.close();
+
+    }
+
+}
