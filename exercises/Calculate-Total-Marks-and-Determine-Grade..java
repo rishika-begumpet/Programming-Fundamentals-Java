671ac0617afec3dcf4283f59c0ccd74b2cb3585c
rishika-begumpet <begumpetrishika@gmail.com>
2026-05-06 14:33:06 +0530

    ✅ Add solution for: Calculate Total Marks and Determine Grade.

---
+++ exercises/Calculate-Total-Marks-and-Determine-Grade..java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int totalMarks = 0;
+        for(int i = 1; i <= 5 ; i++ ){
+           int subject = scanner.nextInt();
+            if(subject > 0 && subject < 100){
+                totalMarks += subject ;
+            }else{
+                System.out.println("Invalid input");
+                return;
+            }
+        }
+        System.out.println( "Total Marks: " + totalMarks);
+        if(totalMarks >= 450){
+            System.out.println ("Grade: A");
+        }else if(totalMarks >= 400){
+            System.out.println( "Grade: B");
+        }else if(totalMarks >= 350){
+            System.out.println("Grade: C");
+        }else if(totalMarks >= 300){
+            System.out.println("Grade: D");
+        }else {
+            System.out.println("Grade: F");
+        } 
+        scanner.close();
+    }
+}
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
